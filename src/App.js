7ac45af8e47794d863fc3ff0d93c1bc3a4aef5fe
Kapil Sharma <sharmakapil1341@gmail.com>
2024-07-07 12:56:26 +0530

    Title change

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
 import FormText from './FormText';
-import About from './About';
 
 function App() {
   return (
     <>
     <Navbar title="TackOn" about="About Us"></Navbar>
     <div className="container my-3">
-      <FormText heading="Enter the text to analyze below"></FormText>
+      <FormText heading="Enter the text to convert below"></FormText>
     </div>
     </>
   );
